b8283b65761ef6c9308c7583bcc4f7be50b51039
Ritika Chandwani <ritikachandwani09@gmail.com>
2022-11-02 20:10:01 +0530

    Frontend created

---
+++ form.js
@@ -0,0 +1,115 @@
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == x.length - 1) {
+    document.getElementById("nextBtn").innerHTML = "Calculate!";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n);
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x,
+    y,
+    i,
+    valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    // if (y[i].value == "") {
+    //   // add an "invalid" class to the field:
+    //   y[i].className += " invalid";
+    //   // and set the current valid status to false:
+    //   valid = false;
+    // }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i,
+    x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
+}
+function toggleField(hideObj, showObj) {
+  hideObj.disabled = true;
+  hideObj.style.display = "none";
+  showObj.disabled = false;
+  showObj.style.display = "inline";
+  showObj.focus();
+}
+function enable_fixed_setpoints() {
+  console.log(document.getElementById("carb").disabled);
+  document.getElementById("carb").disabled =
+    !document.getElementById("carb").disabled;
+  document.getElementById("bicarb").disabled =
+        !document.getElementById("bicarb").disabled;
+    
+    console.log(document.getElementById("carb").disabled)
+    if (document.getElementById("carb").disabled === false) {
+        document.getElementById("carb_endpt").disabled =
+            false;
+    } else document.getElementById("carb_endpt").disabled = true;
+    if (document.getElementById("bicarb").disabled === false) {
+        document.getElementById("bicarb_endpt").disabled =
+            false;
+    } else document.getElementById("bicarb_endpt").disabled = true;
+  // document.getElementsByName("carb_endpt").disabled = !document.getElementsByName("carb_endpt").disabled;
+  // document.getElementsByName("bicarb_endpt").disabled = !document.getElementsByName("bicarb_endpt").disabled;
+}
+function enable_carb_endpt() {
+    {
+       
+        document.getElementById("carb_endpt").disabled =
+    !document.getElementById("carb_endpt").disabled;
+    }
+
+}
+function enable_bicarb_endpt() {
+   
+  document.getElementById("bicarb_endpt").disabled =
+    !document.getElementById("bicarb_endpt").disabled;
+}
